6b9caf4a7fe066d709fbd89485ad2f4d038403e2
Venya <venyadurgam25@gmail.com>
2025-04-08 22:23:52 -0500

    Updated main.py file

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+
+import psycopg2
+import requests
+import simplejson as json
+from confluent_kafka import SerializingProducer
+
+BASE_URL = 'https://randomuser.me/api/?nat=gb'
+PARTIES = ["Management Party", "Savior Party", "Tech Republic Party"]
+random.seed(42)
+def generate_voter_data():
+    response = requests.get(BASE_URL)
+    if response.status_code == 200:
+        user_data = response.json()['results'][0]
+        return {
+            "voter_id": user_data['login']['uuid'],
+            "voter_name": f"{user_data['name']['first']} {user_data['name']['last']}",
+            "date_of_birth": user_data['dob']['date'],
+            "gender": user_data['gender'],
+            "nationality": user_data['nat'],
+            "registration_number": user_data['login']['username'],
+            "address": {
+                "street": f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}",
+                "city": user_data['location']['city'],
+                "state": user_data['location']['state'],
+                "country": user_data['location']['country'],
+                "postcode": user_data['location']['postcode']
+            },
+            "email": user_data['email'],
+            "phone_number": user_data['phone'],
+            "cell_number": user_data['cell'],
+            "picture": user_data['picture']['large'],
+            "registered_age": user_data['registered']['age']
+        }
+    else:
+        return "Error fetching data"
+    
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres")
+    cur = conn.cursor()
