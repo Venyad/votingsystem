7384346df40dae1a82de75c618a01da2f8f7adf0
Venya <venyadurgam25@gmail.com>
2025-04-12 16:01:10 -0500

    Updated streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -90,7 +90,38 @@ def split_frame(input_df, rows):
     df = [input_df.loc[i: i + rows - 1, :] for i in range(0, len(input_df), rows)]
     return df
 
-st.write("REALTIME")
+def paginate_table(table_data):
+    top_menu = st.columns(3)
+    with top_menu[0]:
+        sort = st.radio("Sort Data", options=["Yes", "No"], horizontal=1, index=1)
+    if sort == "Yes":
+        with top_menu[1]:
+            sort_field = st.selectbox("Sort By", options=table_data.columns)
+        with top_menu[2]:
+            sort_direction = st.radio(
+                "Direction", options=["⬆️", "⬇️"], horizontal=True
+            )
+        table_data = table_data.sort_values(
+            by=sort_field, ascending=sort_direction == "⬆️", ignore_index=True
+        )
+    pagination = st.container()
+
+    bottom_menu = st.columns((4, 1, 1))
+    with bottom_menu[2]:
+        batch_size = st.selectbox("Page Size", options=[10, 25, 50, 100])
+    with bottom_menu[1]:
+        total_pages = (
+            int(len(table_data) / batch_size) if int(len(table_data) / batch_size) > 0 else 1
+        )
+        current_page = st.number_input(
+            "Page", min_value=1, max_value=total_pages, step=1
+        )
+    with bottom_menu[0]:
+        st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+
+    pages = split_frame(table_data, batch_size)
+    pagination.dataframe(data=pages[current_page - 1], use_container_width=True)
+
 def update_data():
     # Placeholder to display last refresh time
     last_refresh = st.empty()
