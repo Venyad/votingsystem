b69e4bb73fdc4dc1f11510b41ae81aa2f18793bf
Venya <venyadurgam25@gmail.com>
2025-04-12 15:12:49 -0500

    Updated streamlit-app.py

---
+++ streamlit-app.py
@@ -0,0 +1,17 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import simplejson as json
+import streamlit as st
+from kafka import KafkaConsumer
+from streamlit_autorefresh import st_autorefresh
+import psycopg2
+
+def create_kafka_consumer(topic_name):
+    consumer = KafkaConsumer(
+        topic_name,
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+    return consumer
