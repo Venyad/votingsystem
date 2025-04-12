674c10063b33e1c4226682cdd955d055ed62ee13
Venya <venyadurgam25@gmail.com>
2025-04-12 15:30:34 -0500

    Updated streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -15,3 +15,36 @@ def create_kafka_consumer(topic_name):
         auto_offset_reset='earliest',
         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
     return consumer
+
+@st.cache_data
+def fetch_voting_stats():
+    # Connect to PostgreSQL database
+    conn = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres")
+    cur = conn.cursor()
+
+    # Fetch total number of voters
+    cur.execute("""
+        SELECT count(*) voters_count FROM voters
+    """)
+    voters_count = cur.fetchone()[0]
+
+    # Fetch total number of candidates
+    cur.execute("""
+        SELECT count(*) candidates_count FROM candidates
+    """)
+    candidates_count = cur.fetchone()[0]
+
+    return voters_count, candidates_count
+
+# Function to fetch data from Kafka
+def fetch_data_from_kafka(consumer):
+    # Poll Kafka consumer for messages within a timeout period
+    messages = consumer.poll(timeout_ms=1000)
+    data = []
+
+    # Extract data from received messages
+    for message in messages.values():
+        for sub_message in message:
+            data.append(sub_message.value)
+    return data
+st.write("REALTIME")
