2fd8487503eb8e148b075153cb6a64501dec9f00
Venya <venyadurgam25@gmail.com>
2025-04-11 22:03:18 -0500

    Updated voting.py file

--- voting.py
+++ voting.py
@@ -7,3 +7,43 @@ import simplejson as json
 from confluent_kafka import Consumer, KafkaException, KafkaError, SerializingProducer
 
 from main import delivery_report
+
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+}
+
+consumer = Consumer(conf | {
+    'group.id': 'voting-group',
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': False
+})
+
+producer = SerializingProducer(conf)
+
+
+def consume_messages():
+    result = []
+    consumer.subscribe(['candidates_topic'])
+    try:
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+            elif msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+            else:
+                result.append(json.loads(msg.value().decode('utf-8')))
+                if len(result) == 3:
+                    return result
+
+            # time.sleep(5)
+    except KafkaException as e:
+        print(e)
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres")
+    cur = conn.cursor()
